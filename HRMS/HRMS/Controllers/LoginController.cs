39e7939bba9e70350831fd526d13fcf11f039b4f
siewmuncra <siewmun.cra@hotmail.com>
2018-12-06 01:07:30 +0800

    init commit

---
+++ HRMS/HRMS/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HRMS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRMS.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel model)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
