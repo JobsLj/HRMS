4eafca08fcc629e486ca4318f4dca446d320ce3f
siewmuncra <siewmun.cra@hotmail.com>
2019-01-20 22:18:15 +0800

    Collapsable sidebar

--- HRMS/HRMS/wwwroot/js/site.js
+++ HRMS/HRMS/wwwroot/js/site.js
@@ -1,1 +1,26 @@
 ﻿
+// Collapse/Expand icon
+$('#collapse-icon').addClass('fa-angle-double-left');
+
+// Collapse click
+$('[data-toggle=sidebar-collapse]').click(function () {
+    SidebarCollapse();
+});
+
+function SidebarCollapse() {
+    $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed');
+    $('.menu-collapsed').toggleClass('d-none');
+    $('.sidebar-submenu').toggleClass('d-none');
+    $('.submenu-icon').toggleClass('d-none');
+
+    // Treating d-flex/d-none on separators with title
+    var SeparatorTitle = $('.sidebar-separator-title');
+    if (SeparatorTitle.hasClass('d-flex')) {
+        SeparatorTitle.removeClass('d-flex');
+    } else {
+        SeparatorTitle.addClass('d-flex');
+    }
+
+    // Collapse/Expand icon
+    $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
+}
